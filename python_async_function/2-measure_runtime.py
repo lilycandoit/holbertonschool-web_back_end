88929866123a366e7bac5a6a7bb6711c9641bd8a
lily <hueduong288@gmail.com>
2025-08-02 21:07:47 +1000

    calculate the everage runtime

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Measure the average runtime of wait_n."""
+
+import time
+import asyncio
+from typing import Callable
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure total runtime of wait_n(n, max_delay),
+    then return average time per call."""
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    return total_time / n
