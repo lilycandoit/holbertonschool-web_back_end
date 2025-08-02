e5077ba35059a9d992a877c6713cc33f121ce63c
lily <hueduong288@gmail.com>
2025-08-02 21:48:31 +1000

    practice Task scheduling patterns with create_task

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn task_wait_random n times and return list of results in order of completion."""
+    """Spawn task_wait_random n times and return
+    list of results in order of completion."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays: List[float] = []
 
