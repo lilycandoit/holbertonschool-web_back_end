5fdd63a29e9a1e58eebb246090146e383c39f310
lily <hueduong288@gmail.com>
2025-08-11 21:36:13 +1000

    Simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Simple pagination on a CSV dataset of popular baby names.
+"""
+
+import csv
+import math
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Use assert to verify that both arguments are integers greater than 0.
+
+        Use index_range to find the correct indexes.
+
+        If the input arguments are out of range
+        for the dataset, an empty list should be returned.
+
+        Return page of the dataset.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = self.dataset()
+        start, end = index_range(page, page_size)
+
+        print(len(data))
+
+        if start >= len(data):
+            return []
+        return data[start:end]
