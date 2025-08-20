e6ed07ea63055e273cbfc02c4e784b4d1ae88177
lily <hueduong288@gmail.com>
2025-08-20 20:11:25 +1000

    create a report object

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const report = {
+    allEmployees: { ...employeesList },
+
+    getNumberOfDepartments(allEmployees) {
+      return Object.keys(allEmployees).length;
+    },
+  };
+  return report;
+}
