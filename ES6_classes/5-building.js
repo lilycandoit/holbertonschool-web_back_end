b5ce4c49dbbadb44cef59b8b9365f43f431bfdcb
lily <hueduong288@gmail.com>
2025-08-21 15:07:16 +1000

    simumuate abstract class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    // Ensure that any subclass implements evacuationWarningMessage
+    if (
+      this.constructor !== Building &&
+      this.evacuationWarningMessage === undefined
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+}
