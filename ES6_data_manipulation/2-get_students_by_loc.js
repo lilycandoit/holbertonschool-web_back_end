ede3c747d5b28b99f89d09d2274253b2f3e2308d
lily <hueduong288@gmail.com>
2025-08-21 21:26:53 +1000

    eslint error

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,5 +2,5 @@ export default function getStudentsByLocation(students, city) {
 	if (!Array.isArray(students) || typeof city !== 'string') {
 		return [];
 	}
-	return students.filter(student => student.location === city);
+	return students.filter((student) => (student.location === city));
 }
