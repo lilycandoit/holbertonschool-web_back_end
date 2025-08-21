72ab8150318e53732e8687749c062f430e6aaf82
lily <hueduong288@gmail.com>
2025-08-21 22:15:47 +1000

    combine filter, map, find and ternary operator

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, updates) {
+  if (!Array.isArray(students) || typeof city !== 'string' || !Array.isArray(updates)) {
+    return [];
+  }
+
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const found = updates.find((update) => update.studentId === student.id);
+
+      return {
+        ...student, // spread existing student properties
+        grade: found ? found.grade : 'N/A', // assign grade if found, else "N/A"
+      };
+    })
+}
