6903d79aeeffc8547dd71d5b17894bc636d23912
lily <hueduong288@gmail.com>
2025-08-22 17:55:35 +1000

    task3 - Promise all

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default async function handleProfileSignup() {
    try {
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(photo.body, user.firstName, user.lastName);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   } catch (err) {
     console.log('Signup system offline');
   }
