5ff2e1ce1f0d49dcdc3e1dc0340600ece0a93533
lily <hueduong288@gmail.com>
2025-08-27 18:39:03 +1000

    return the value returned by the promise that resolved the first

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,6 +7,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       return results.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : String(result.reason),
-      }))
-    })
+      }));
+    });
 }
