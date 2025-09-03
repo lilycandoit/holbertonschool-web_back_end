bfacd83127e9ff14599ac7a91eed3aa4a3efd6dc
lily <hueduong288@gmail.com>
2025-09-03 14:11:29 +1000

    using child process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name? ');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-let input = '';
-
-process.stdin.on('data', (data) => {
-  input += data.trim();
-  process.stdout.write(`Your name is: ${input}\n`);
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
 process.stdin.on('end', () => {
