4f61cacecdbde7296c8ed84074f5571bc2b0d312
lily <hueduong288@gmail.com>
2025-09-04 10:46:12 +1000

    7. Create a more complex HTTP server using Express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((output) => {
+      res.type('text/plain');
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch(() => {
+      res.type('text/plain');
+      res.end('Cannot load the database');
+    });
+});
+
+app.listen(1245);
+module.exports = app;
