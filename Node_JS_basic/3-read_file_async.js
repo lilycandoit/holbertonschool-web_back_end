84b462c18545d3e01df9629bf34cbd7bdf375d5b
lily <hueduong288@gmail.com>
2025-09-04 10:11:54 +1000

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -4,7 +4,10 @@ async function countStudents(path) {
   try {
     const data = await fs.readFile(path, 'utf8');
 
-    const lines = data.trim().split('\n').filter(Boolean);
+    const lines = data
+      .trim()
+      .split('\n')
+      .filter(Boolean);
     // a trick to exclude empty for invalid row
 
     if (lines <= 1) {
@@ -26,13 +29,14 @@ async function countStudents(path) {
       groups[field].push(firstname);
     });
 
-    console.log(`Number of students: ${total}`);
+    let output = `Number of students: ${total}\n`;
 
     for (const [field, list] of Object.entries(groups)) {
-      console.log(
-        `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`,
-      );
+      output += `Number of students in ${field}: ${
+        list.length
+      }. List: ${list.join(', ')}\n`;
     }
+    return output.trim();
   } catch (error) {
     throw new Error('Cannot load the database');
   }
---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url == '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url == '/students') {
+    res.write('This is the list of our students\n');
+
+    countStudents(process.argv[2])
+      .then((output) => {
+        res.end(output);
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
+      })
+  } else {
+    res.end('Not found');
+  }
+})
+
+app.listen(1245)
+
+module.exports = app
