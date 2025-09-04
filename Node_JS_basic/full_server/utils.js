442743050ed707b85d00a8728b201167e08e5c76
lily <hueduong288@gmail.com>
2025-09-04 11:51:52 +1000

    create utils.js to read database

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,33 @@
+import fs from 'fs/promises';
+
+export async function readDatabase(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const lines = data
+      .trim()
+      .split('\n')
+      .filter(Boolean); // shorthand to make sure no empty invalid lines
+    // because empty string is Falsy in JS
+    // same as: .filter(line => line != '')
+
+    const [, ...rows] = lines;
+
+    const studentsByField = {};
+
+    rows.forEach((row) => {
+      const [firstname, , , field] = row.split(',');
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
+      }
+      studentsByField[field].push(firstname);
+    });
+
+    console.log(studentsByField);
+    return studentsByField;
+
+  } catch (error) {
+    throw new Error(`Error reading database: ${error.message}`);
+  }
+}
+
+readDatabase('../database.csv');
